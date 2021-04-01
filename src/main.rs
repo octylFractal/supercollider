0afc810de6846f6ffcb8ca26ddd7da67ce9d6aab
Octavia Togami <octavia.togami@gmail.com>
2021-03-31 22:19:46 -0700

    Optimize a bit

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use crate::gjk::GJK;
 use crate::rect::Rect;
 use crate::shape::ConvexShape;
 use std::iter::Once;
+use std::time::Instant;
 
 mod circle;
 mod gjk;
@@ -14,13 +15,16 @@ mod rect;
 mod shape;
 
 fn main() {
+    let random_shapes = (0..10_000_000).into_iter()
+        .map(|_| (random_shape(), random_shape()))
+        .collect::<Vec<_>>();
+    let start = Instant::now();
     let mut collision_hash = 1;
-    for _ in 0..10_000_000 {
-        let shape_a = random_shape();
-        let shape_b = random_shape();
+    for (shape_a, shape_b) in random_shapes {
         collision_hash += shape_a.iter().check_collision(shape_b.iter()) as i32;
     }
-    eprintln!("{:?}", collision_hash);
+    let elapsed = start.elapsed();
+    eprintln!("{:?} 10 million in {:?}", collision_hash, elapsed);
 }
 
 fn random_shape() -> RandomShape {
